2156be5076d22b22ce87bd87be29898a16d218c4
Treinamento <Treinamento@RD723896.raiadrogasil.com>
2020-02-07 08:19:21 -0300

    implementando os pontos zero

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let tela = document.getElementById('tela');
+let d0 = document.getElementById('d0');
 let d1 = document.getElementById('d1');
 let d2 = document.getElementById('d2');
 let d3 = document.getElementById('d3');
@@ -8,6 +9,7 @@ let d6 = document.getElementById('d6');
 let d7 = document.getElementById('d7');
 let d8 = document.getElementById('d8');
 let d9 = document.getElementById('d9');
+let dPonto = document.getElementById('dPonto');
 let dMais = document.getElementById('mais');
 let dMenos = document.getElementById('menos');
 let dMult = document.getElementById('mult');
@@ -22,6 +24,11 @@ const digitandoTecla = (event) => {
     tela.insertAdjacentHTML('beforeend',valor)
 };
 
+const digitandoTeclaPonto = (event) => {
+    let valor = event.target.innerText;
+    tela.insertAdjacentHTML('beforeend',".")
+};
+
 const limparTela = (event) => {
     tela.innerHTML = "";
 }
@@ -33,19 +40,21 @@ const results = (event) => {
     tela.innerHTML = resul;
 }
 
-d1.addEventListener('click', digitandoTecla)
-d2.addEventListener('click', digitandoTecla)
-d3.addEventListener('click', digitandoTecla)
-d4.addEventListener('click', digitandoTecla)
-d5.addEventListener('click', digitandoTecla)
-d6.addEventListener('click', digitandoTecla)
-d7.addEventListener('click', digitandoTecla)
-d8.addEventListener('click', digitandoTecla)
-d9.addEventListener('click', digitandoTecla)
+d0.addEventListener('click', digitandoTecla);
+d1.addEventListener('click', digitandoTecla);
+d2.addEventListener('click', digitandoTecla);
+d3.addEventListener('click', digitandoTecla);
+d4.addEventListener('click', digitandoTecla);
+d5.addEventListener('click', digitandoTecla);
+d6.addEventListener('click', digitandoTecla);
+d7.addEventListener('click', digitandoTecla);
+d8.addEventListener('click', digitandoTecla);
+d9.addEventListener('click', digitandoTecla);
 
+dPonto.addEventListener('click', digitandoTeclaPonto);
 dMais.addEventListener('click', digitandoTecla);
 dMenos.addEventListener('click', digitandoTecla);
 dMult.addEventListener('click', digitandoTecla);
 dDiv.addEventListener('click', digitandoTecla);
 limpar.addEventListener('click', limparTela);
-resultado.addEventListener('click', results)
+resultado.addEventListener('click', results);
